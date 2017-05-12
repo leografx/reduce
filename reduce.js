332928d032bbefec7cfded9de5e21a8fa57e0afa
leografx <leografx@gmail.com>
2017-05-12 15:25:21 -0400

    added reduce

--- reduce.js
+++ reduce.js
@@ -1,7 +1,14 @@
 const calendar = [
-	{ scheduled:10, planned:0, committed:0, balance:0 },
+	{ scheduled:10, planned:0, committed:10, balance:0 },
 	{ scheduled:10, planned:0, committed:0, balance:0 },
 ]
 
 const init = [{ scheduled:0, planned:0, committed:0, balance:0 }]
 
+let cal = calendar.reduce( (calendar,current,i) => {
+	current.balance = calendar[i].balance + current.scheduled + current.planned - current.committed
+	calendar.push(current)
+	return calendar
+},init)
+
+console.log(JSON.stringify(cal))
