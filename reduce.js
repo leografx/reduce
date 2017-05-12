ae49db14f73817731d7923a0dc97cf27071f9281
leografx <leografx@gmail.com>
2017-05-12 15:05:53 -0400

    reduce function for planner

---
+++ reduce.js
@@ -0,0 +1,7 @@
+const calendar = [
+	{ scheduled:10, planned:0, committed:0, balance:0 },
+	{ scheduled:10, planned:0, committed:0, balance:0 },
+]
+
+const init = [{ scheduled:0, planned:0, committed:0, balance:0 }]
+
